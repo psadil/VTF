fc8db9c6c76ba5b0dc5f825db341c1be7615c145
Patrick Sadil <psadil@gmail.com>
2017-06-12 18:53:33 -0400

    updates

--- lib/utils/deg2pix.m
+++ lib/utils/deg2pix.m
@@ -3,6 +3,6 @@ function pixels = deg2pix(deg, window)
 pixelsPerDegree = window.width / ...
     (2 * (atand(window.screenWidthCM / (2*window.vDistCM))));
 
-pixels = deg * pixelsPerDegree;
+pixels = ceil(deg * pixelsPerDegree);
 
 end
