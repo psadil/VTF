d64db5cc6f9a78b8156a2355f7a07e2aedbe50b5
Patrick Sadil <psadil@gmail.com>
2018-03-15 12:38:37 -0400

    made hardest dim easier (0.1)

--- lib/PsychSetup/setupStaircase.m
+++ lib/PsychSetup/setupStaircase.m
@@ -2,7 +2,7 @@ function stairs = setupStaircase( delta_luminance_guess )
 
 
 % these are the luminance changes to consider
-stairs.options = (0.05:.01:.6)';
+stairs.options = (0.1:.01:.6)';
 stairs.first_trial = delta_luminance_guess;
 
 stairs.luminance_difference = stairs.first_trial;
